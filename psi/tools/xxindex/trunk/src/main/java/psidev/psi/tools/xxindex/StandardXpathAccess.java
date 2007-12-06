7a75cd9de6ef8b4cf260b78fe1cb4c349a0ed990
martlenn <martlenn@275e8d40-e116-0410-a579-d6759ba1b15c>
2007-12-06 13:28:17 +0000

    - Introduced the inclusion set for the xpath index at this level as well.

--- psi/tools/xxindex/trunk/src/main/java/psidev/psi/tools/xxindex/StandardXpathAccess.java
+++ psi/tools/xxindex/trunk/src/main/java/psidev/psi/tools/xxindex/StandardXpathAccess.java
@@ -7,10 +7,7 @@ import psidev.psi.tools.xxindex.index.XpathIndex;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Collections;
+import java.util.*;
 
 /**
  * Author: Florian Reisinger
@@ -24,11 +21,39 @@ public class StandardXpathAccess implements XpathAccess {
 
 
     ////////////////////
-    // Constructor
+    // Constructors
 
+    /**
+     * This constructor creates an xpath index from the specified XML file,
+     * and will include all encountered xpaths in this index.
+     *
+     * @param file  File with the XML file to index
+     * @throws IOException  when the file could not be accessed
+     */
     public StandardXpathAccess(File file) throws IOException {
+        this(file, null);
+    }
+
+    /**
+     * This constructor creates an xpath index from the specified XML file,
+     * and takes a set of xpaths to include in this index.
+     * All xpaths that do not correspond to one of the xpaths included
+     * in this set will be ignored and therefore omitted from the index!
+     *
+     * @param file  File with the XML file to index.
+     * @param aXpathInclusionSet    Set with the String representation
+     *                              of the xpaths to include in the index.
+     *                              <b>Note</b> that these xpaths should have
+     *                              their trailing '/' removed!
+     *                              <b>Also note</b> that any xpath not included
+     *                              in this list will <b>not</b> be added
+     *                              to the index! Can be 'null' to ensure
+     *                              inclusion of all xpaths.
+     * @throws  IOException when the file could not be accessed
+     */
+    public StandardXpathAccess(File file, Set<String> aXpathInclusionSet) throws IOException {
         this.file = file;
-        this.index = XmlXpathIndexer.buildIndex(new FileInputStream(file));
+        this.index = XmlXpathIndexer.buildIndex(new FileInputStream(file), aXpathInclusionSet);
         this.extractor = new StandardXmlElementExtractor();
         String enc = extractor.detectFileEncoding(file.toURL());
         extractor.setEncoding(enc);
