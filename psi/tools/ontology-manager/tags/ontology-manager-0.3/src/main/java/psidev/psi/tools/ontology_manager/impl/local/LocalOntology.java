64417d7b808ff16e931a25cc0f1a9df62d793dd5
martlenn <martlenn@275e8d40-e116-0410-a579-d6759ba1b15c>
2007-12-06 14:38:27 +0000

    [maven-release-plugin]  copy for tag ontology-manager-0.3

---
+++ psi/tools/ontology-manager/tags/ontology-manager-0.3/src/main/java/psidev/psi/tools/ontology_manager/impl/local/LocalOntology.java
@@ -0,0 +1,119 @@
+package psidev.psi.tools.ontology_manager.impl.local;
+
+import psidev.psi.tools.ontology_manager.impl.local.model.OntologyTerm;
+import psidev.psi.tools.ontology_manager.interfaces.OntologyAccess;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Collection;
+import java.net.URI;
+import java.net.URL;
+import java.net.MalformedURLException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Author: Florian Reisinger
+ * Date: 07-Aug-2007
+ */
+public class LocalOntology implements OntologyAccess {
+
+    public static final Log log = LogFactory.getLog( LocalOntology.class );
+
+    private Ontology ontology;
+    String ontologyID;
+
+    public LocalOntology() {
+        log.info( "Creating new LocalOntology..." );
+        ontology = null;
+    }
+
+
+    public void loadOntology( String ontologyID, String name, String version, String format, URI uri) throws OntologyLoaderException {
+        this.ontologyID = ontologyID;
+
+        // first check the format
+        if ( "OBO".equals( format ) ) {
+            if ( uri == null ) {
+                throw new IllegalArgumentException( "The given CVSource doesn't have a URL" );
+            } else {
+                URL url;
+                try {
+                    url = uri.toURL();
+                } catch ( MalformedURLException e ) {
+                    throw new IllegalArgumentException( "The given CVSource doesn't have a valid URL: " + uri );
+                }
+
+                // parse the URL and load the ontology
+                OboLoader loader = new OboLoader();
+                try {
+                    ontology = loader.parseOboFile( url );
+                } catch (OntologyLoaderException e) {
+                    throw new OntologyLoaderException("OboFile parser failed with Exception: ", e);
+                }
+            }
+        } else {
+            throw new OntologyLoaderException( "Unsupported ontology format: " + format );
+        }
+
+        log.info("Successfully created LocalOntology from values: ontology="
+                + ontologyID + " name=" + name + " version=" + version + " format=" + format + " location=" + uri);
+
+    }
+
+    public Set<String> getValidIDs( String id, boolean allowChildren, boolean useTerm) {
+        Set<String> terms = new HashSet<String>();
+
+        OntologyTerm resultTerm = ontology.search(id); // will return null if no such term found
+
+        if ( resultTerm != null ) {
+            if ( useTerm ) {
+                terms.add(resultTerm.getId());
+            }
+            if ( allowChildren ) {
+                Set<OntologyTerm> childTerms = resultTerm.getAllChildren();
+                for (OntologyTerm childTerm : childTerms) {
+                    terms.add(childTerm.getId());
+                }
+            }
+        } else {
+            log.warn("No matching entries in local ontology '" + ontologyID
+                    + "' for term '" + id + "'. Returning empty set of valid terms.");
+        }
+        log.debug( "Returning " + terms.size() + " valid IDs for ontology= " + ontologyID + " id=" + id + " allowChilrden=" + allowChildren + " useTerm=" + useTerm );
+        return terms;
+    }
+
+    public boolean isObsoleteID(String id) {
+        OntologyTerm term = ontology.search( id );
+        if ( term == null ) {
+            throw new IllegalStateException("Checking obsolete on non existing term!");
+        }
+        log.debug( "Term '" + id + "' obsolete? " + term.isObsolete() );
+        return term.isObsolete();
+    }
+
+    public String getTermNameByID(String id) {
+        String result = null;
+        // ToDo: check if shortName (or fullName) is the one to use
+        OntologyTerm term = ontology.search( id );
+        if ( term != null ) {
+            result = term.getShortName();
+        }
+        log.debug( "Name for term '" + id + "' is: " + term.getShortName() );
+        return result;
+    }
+
+    public Set<String> getDirectParentsIDs(String id) {
+        Set<String> result = new HashSet<String>();
+        Collection<OntologyTerm> terms = ontology.search( id ).getParents();
+        for (OntologyTerm term : terms) {
+            result.add( term.getId() );
+        }
+        log.debug( "Term '" + id + "' has " + result.size() + " parent terms." );
+        return result;
+    }
+
+
+}
