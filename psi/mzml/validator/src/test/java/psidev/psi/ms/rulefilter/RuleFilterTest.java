231b9f606ffcbac994277f3c489ba3a673bbcde0
smartinez21 <smartinez21@275e8d40-e116-0410-a579-d6759ba1b15c>
2012-01-05 10:54:49 +0000

    print rule filter moved to filter manager class

--- psi/mzml/validator/src/test/java/psidev/psi/ms/rulefilter/RuleFilterTest.java
+++ psi/mzml/validator/src/test/java/psidev/psi/ms/rulefilter/RuleFilterTest.java
@@ -10,18 +10,8 @@ import javax.xml.bind.JAXBException;
 
 import org.junit.Test;
 
-import psidev.psi.ms.rulefilter.jaxb.CvMappingRuleCondition;
-import psidev.psi.ms.rulefilter.jaxb.CvMappingRuleToSkip;
-import psidev.psi.ms.rulefilter.jaxb.ObjectRuleCondition;
-import psidev.psi.ms.rulefilter.jaxb.ObjectRuleToSkip;
-import psidev.psi.ms.rulefilter.jaxb.ReferencedRules;
-import psidev.psi.ms.rulefilter.jaxb.RuleFilter;
-import psidev.psi.ms.rulefilter.jaxb.RulesToSkipRef;
-import psidev.psi.ms.rulefilter.jaxb.UserCondition;
-import psidev.psi.ms.rulefilter.jaxb.UserOption;
-
 public class RuleFilterTest {
-	private static final String FILE_RULE_FILTER = "ruleFilter_MS.xml";
+	private static final String FILE_RULE_FILTER = "ruleFilterMIAPE.xml";
 
 	@Test
 	public void ruleFilterTest() {
@@ -29,7 +19,7 @@ public class RuleFilterTest {
 		try {
 			File file = new File(FILE_RULE_FILTER);
 			final RuleFilterManager filterManager = new RuleFilterManager(file);
-			printRuleFilter(filterManager.getFilter());
+			filterManager.printRuleFilter();
 			HashMap<String, String> selectedOptions = new HashMap<String, String>();
 			selectedOptions.put(MaldiOrEsiCondition.getID(), MaldiOrEsiCondition.ESI.getOption());
 			filterManager.getCVMappingRulesToSkipByUserOptions(selectedOptions);
@@ -59,65 +49,4 @@ public class RuleFilterTest {
 		}
 	}
 
-	private void printRuleFilter(RuleFilter ruleFilter) {
-		for (UserCondition condition : ruleFilter.getUserConditions().getUserCondition()) {
-			System.out.println("\nCondition: " + condition.getId());
-			for (UserOption option : condition.getUserOption()) {
-				System.out.println("Option " + option.getId());
-				for (CvMappingRuleToSkip cvMappingRule : option.getCvMappingRuleToSkip()) {
-					System.out.println("mapping rule id=" + cvMappingRule.getId());
-				}
-				for (ObjectRuleToSkip objectRule : option.getObjectRuleToSkip()) {
-					System.out.println("object rule id=" + objectRule.getId());
-				}
-				if (option.getRulesToSkipRef() != null) {
-					for (RulesToSkipRef ruleSetReference : option.getRulesToSkipRef()) {
-						if (ruleFilter.getReferences() != null) {
-							for (ReferencedRules referencedRuleSet : ruleFilter.getReferences()
-									.getReferencedRules()) {
-								if (referencedRuleSet.getId().equals(ruleSetReference.getRef())) {
-									for (CvMappingRuleToSkip cvMappingRule : referencedRuleSet
-											.getCvMappingRuleToSkip()) {
-										System.out.println("mapping rule id="
-												+ cvMappingRule.getId());
-									}
-									for (ObjectRuleToSkip objectRule : referencedRuleSet
-											.getObjectRuleToSkip()) {
-										System.out.println("object rule id=" + objectRule.getId());
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		if (ruleFilter.getObjectRuleConditions() != null)
-			for (ObjectRuleCondition objectRuleCondition : ruleFilter.getObjectRuleConditions()
-					.getObjectRuleCondition()) {
-				System.out.println("\nObject rule condition: " + objectRuleCondition.getId()
-						+ " isValid=" + objectRuleCondition.isValid());
-				for (ObjectRuleToSkip objectRule : objectRuleCondition.getObjectRuleToSkip()) {
-					System.out.println("\t\tobject rule to skip=" + objectRule.getId());
-				}
-				for (CvMappingRuleToSkip cvMappingRule : objectRuleCondition
-						.getCvMappingRuleToSkip()) {
-					System.out.println("\t\tcvMapping rule to skip=" + cvMappingRule.getId());
-				}
-			}
-		if (ruleFilter.getCvMappingRuleConditions() != null)
-			for (CvMappingRuleCondition cvMappingCondition : ruleFilter
-					.getCvMappingRuleConditions().getCvMappingRuleCondition()) {
-				System.out.println("\ncvMapping rule condition: " + cvMappingCondition.getId()
-						+ " isValid=" + cvMappingCondition.isValid());
-				for (ObjectRuleToSkip objectRule : cvMappingCondition.getObjectRuleToSkip()) {
-					System.out.println("\t\tobject rule to skip=" + objectRule.getId());
-				}
-				for (CvMappingRuleToSkip cvMappingRule : cvMappingCondition
-						.getCvMappingRuleToSkip()) {
-					System.out.println("\t\tcvMapping rule to skip=" + cvMappingRule.getId());
-				}
-			}
-
-	}
 }
